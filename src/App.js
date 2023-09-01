d4808ba8020a8e0ec7dc7cec9d7352c3fcabfa7d
Ahmed Hamdy <94504141+Ahmed-Hamdy101@users.noreply.github.com>
2023-09-01 13:48:46 +0000

    add components 📦 and randomizeColor 🎠

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import RandomizeColors from "./projects/app/Randomize_colors";
+
+function App() {
+  return (
+    <div>
+      <RandomizeColors />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/projects/app/Randomize_colors.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Title from "../components/Title";
+export default function Randomize_colors(){
+  return (
+    <div className='container'>
+    <Title text={"Random-colors"} classes={"mb-4"}/>
+    <button className='btn btn-danger'>click</button>
+    </div>
+
+  )
+}
